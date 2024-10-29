5407f789c79d58632bd002afca6efc7afbcde947
신동근 <donggeun.shin@mirero.co.kr>
2024-10-29 13:35:29 +0900

    [BE] feat: 프로젝트 초기 세팅 및 멀티모듈 설정, FTP-Service 생성

---
+++ Term-Project/BackEnd/FTP-Service/src/main/java/com/mirero/ftpservice/FtpServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mirero.ftpservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FtpServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FtpServiceApplication.class, args);
+	}
+
+}
