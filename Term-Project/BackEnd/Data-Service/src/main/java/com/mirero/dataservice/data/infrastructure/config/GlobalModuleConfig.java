e3e51af8771c3b33c0e1e5eb8698f867d9f6566a
신동근 <donggeun.shin@mirero.co.kr>
2024-11-04 16:45:44 +0900

    [BE] feat: Global Module에 Jasypt 설정 관련 추가 및 Data Service에 Global Module 관련 설정 (Config) Component Scan을 통해서 가져오도록 설정

---
+++ Term-Project/BackEnd/Data-Service/src/main/java/com/mirero/dataservice/data/infrastructure/config/GlobalModuleConfig.java
@@ -0,0 +1,9 @@
+package com.mirero.dataservice.data.infrastructure.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {"com.mirero.globalmodule"})
+public class GlobalModuleConfig {
+}
