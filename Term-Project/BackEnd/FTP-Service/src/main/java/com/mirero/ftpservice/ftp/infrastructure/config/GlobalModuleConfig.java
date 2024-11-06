c4d9e64c034177aa603ca4923ccc3fb0d72e9d28
신동근 <donggeun.shin@mirero.co.kr>
2024-11-06 09:01:10 +0900

    [BE] feat: FTP Service Global Module 관련 설정 이용할 수 있게끔 GlobalModuleConfig 생성

---
+++ Term-Project/BackEnd/FTP-Service/src/main/java/com/mirero/ftpservice/ftp/infrastructure/config/GlobalModuleConfig.java
@@ -0,0 +1,9 @@
+package com.mirero.ftpservice.ftp.infrastructure.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {"com.mirero.globalmodule"})
+public class GlobalModuleConfig {
+}
