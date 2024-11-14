a251f7301d84804c9ae6bf96400cee03489347af
신동근 <donggeun.shin@mirero.co.kr>
2024-11-14 11:40:44 +0900

    [BE] feat: MongoDB 설정 파일 생성

---
+++ Term-Project/BackEnd/Global-Module/src/main/java/com/mirero/globalmodule/config/MongoConfig.java
@@ -0,0 +1,40 @@
+package com.mirero.globalmodule.config;
+
+import com.mirero.globalmodule.property.MongoProperties;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import java.util.Collections;
+import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
+
+@Configuration
+@RequiredArgsConstructor
+public class MongoConfig extends AbstractMongoClientConfiguration {
+
+    private final MongoProperties mongoProperties;
+
+    @Override
+    protected String getDatabaseName() {
+        return mongoProperties.databaseName();
+    }
+
+    @Bean
+    @Override
+    public MongoClient mongoClient() {
+        MongoCredential credential = MongoCredential.createCredential(mongoProperties.userName(),
+            mongoProperties.authDatabase(), mongoProperties.password().toCharArray());
+        ServerAddress serverAddress = new ServerAddress(mongoProperties.host(), mongoProperties.port());
+        return MongoClients.create(
+            MongoClientSettings.builder()
+                .applyToClusterSettings(builder -> builder.hosts(Collections.singletonList(serverAddress)))
+                .credential(credential)
+                .build()
+        );
+    }
+}
