8cbab48912f5c0ad6ea9f6d0064395163f2072f3
신동근 <donggeun.shin@mirero.co.kr>
2024-11-14 11:50:43 +0900

    [BE] feat: 도큐먼트에 공통적으로 사용할 Base 도큐먼트 추가

---
+++ Term-Project/BackEnd/Global-Module/src/main/java/com/mirero/globalmodule/common/document/BaseDocument.java
@@ -0,0 +1,19 @@
+package com.mirero.globalmodule.common.document;
+
+import java.time.LocalDateTime;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+@Getter
+public abstract class BaseDocument {
+
+    @CreatedDate
+    @Field("created_at")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Field("updated_at")
+    private LocalDateTime updatedAt;
+}
